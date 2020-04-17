d05a317c3f8bde4b45288fb5ff8f561a8ede649e
Vikram Paul <vikrampaul1996@gmail.com>
2020-04-17 05:47:50 +0530

    Initial Commit

---
+++ FileUploadMvc/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FileUploadMvc.Models;
+using Microsoft.AspNetCore.Http;
+using System.Runtime.InteropServices;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FileUploadMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(IEnumerable<IFormFile> files)
+        {
+            string Message = string.Empty;
+            try
+            {
+                string FileUploadPath = Convert.ToString(_configuration.GetSection("FileUploadMvc").GetSection("FileUploadPath").Value.Trim());
+                Int64 FileUploadMaxSize = Convert.ToInt64(_configuration.GetSection("FileUploadMvc").GetSection("FileUploadMaxSize").Value);
+                string FileUploadAllowedTypes = Convert.ToString(_configuration.GetSection("FileUploadMvc").GetSection("FileUploadAllowedTypes").Value.ToLower().Trim());
+                string FileUploadName = Convert.ToString(_configuration.GetSection("FileUploadMvc").GetSection("FileUploadName").Value.Trim());
+                Dictionary<string, string> mimeTypeList = getAllowedMimeType(FileUploadAllowedTypes);
+
+                foreach (IFormFile file in files)
+                {
+                    var contentType = file.ContentType;
+                    var fileExtension = Path.GetExtension(file.FileName);
+
+                    if (file.Length <= FileUploadMaxSize)
+                    {
+                        try
+                        {
+                            if (mimeTypeList.ContainsKey(contentType))
+                            {
+                                if (fileExtension == mimeTypeList[contentType])
+                                {
+                                    string filename = string.Concat(Guid.NewGuid(), Path.GetExtension(file.FileName));
+                                    using (FileStream output = System.IO.File.Create(filename))
+                                        await file.CopyToAsync(output);
+
+                                    Message = String.Concat(Message, " Files Uploaded Successfully : ", file.FileName);
+                                }
+                                else
+                                {
+                                    Message = String.Concat(Message, " Upload Unsuccessful! File Content and File extension mismatch : ", file.FileName);
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Message = String.Concat(Message, " ", e.Message, " FileName: ", file.FileName);
+                        }
+                    }
+                    else
+                    {
+                        Message = String.Concat(Message, " File size exceeded max : ", file.FileName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Message = String.Concat(Message, " ", e.Message);
+            }
+            ViewBag.Message = Message;
+            return View("Index");
+        }
+
+        public Dictionary<string, string> getAllowedMimeType(string FileUploadAllowedTypes)
+        {
+            Dictionary<string, string> mimeTypeList = new Dictionary<string, string>();
+            if (FileUploadAllowedTypes.Contains("jpeg"))
+            {
+                mimeTypeList.Add("image/jpeg", "jpeg");
+            }
+            if (FileUploadAllowedTypes.Contains("docx"))
+            {
+                mimeTypeList.Add("application/vnd.openxmlformats-officedocument.wordprocessingml.document", "docx");
+            }
+            if (FileUploadAllowedTypes.Contains("doc"))
+            {
+                mimeTypeList.Add("application/msword", "doc");
+            }
+            if (FileUploadAllowedTypes.Contains("png"))
+            {
+                mimeTypeList.Add("image/png", "png");
+            }
+            if (FileUploadAllowedTypes.Contains("ppt"))
+            {
+                mimeTypeList.Add("application/vnd.ms-powerpoint", "ppt");
+            }
+            if (FileUploadAllowedTypes.Contains("pptx"))
+            {
+                mimeTypeList.Add("application/vnd.openxmlformats-officedocument.presentationml.presentation", "pptx");
+            }
+            if (FileUploadAllowedTypes.Contains("txt"))
+            {
+                mimeTypeList.Add("text/plain", "txt");
+            }
+            if (FileUploadAllowedTypes.Contains("xlsx"))
+            {
+                mimeTypeList.Add("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx");
+            }
+            if (FileUploadAllowedTypes.Contains("zip"))
+            {
+                mimeTypeList.Add("application/zip", "zip");
+            }
+            return mimeTypeList;
+        }
+    }
+}
---
+++ FileUploadMvc/ValidateContentType.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace FileUploadMvc
+{
+    public class ValidateContentType
+    {
+        //private string EnsureCorrectFilename(string filename)
+        //{
+        //    if (filename.Contains("\\"))
+        //        filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+
+        //    return filename;
+        //}
+
+        //private string GetPathAndFilename(string filename)
+        //{
+        //    return this.hostingEnvironment.WebRootPath + "\\uploads\\" + filename;
+        //}
+
+        public static string GetMimeType(byte[] content)
+        {
+            string output = string.Empty;
+            int MaxContent = content.Length;
+            string mime = string.Empty; int result = 0;
+            byte[] buf = new byte[MaxContent];
+            //content.InputStream.Read(document, 0, file.ContentLength);
+            System.UInt32 mimetype;
+            FindMimeFromData(0, null, buf, 256, null, 0, out mimetype, 0);
+            System.IntPtr mimeTypePtr = new IntPtr(mimetype);
+            mime = Marshal.PtrToStringUni(mimeTypePtr);
+            Marshal.FreeCoTaskMem(mimeTypePtr);
+
+            if (mime == "application/pdf")
+            {
+                // upload the File because file is valid  
+                result = 1;
+                output = "Valid";
+            }
+            else
+            {
+                //  file is Invalid  
+                result = 0;
+                output = "Invalid";
+            }
+            return output;
+        }
+
+        [DllImport(@"urlmon.dll", CharSet = CharSet.Auto)]
+        private extern static System.UInt32 FindMimeFromData(uint pBC,
+        [MarshalAs(UnmanagedType.LPStr)] string pwzUrl,
+        [MarshalAs(UnmanagedType.LPArray)] byte[] pBuffer,
+        uint cbSize, [MarshalAs(UnmanagedType.LPStr)] string pwzMimeProposed,
+        uint dwMimeFlags,
+        out uint ppwzMimeOut,
+        uint dwReserverd);
+    }
+}
